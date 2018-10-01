e44760092e6a10ac999a404bac66fe3b33b4a457
Kate Demianenko <kadee1344@gmail.com>
2018-10-01 15:05:39 +0300

    Trouble with QueryKeyMaker cannot handle circular query structures

---
+++ App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import navStyles from './styles/navStyles';
+import Post from './Post';
+import Posts from './Posts';
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://api.graph.cool/relay/v1/cjmq0qqyg39rg0147n4k0typm"
+  }),
+  cache
+});
+
+console.log(client);
+
+class App extends React.Component {
+  static navigationOptions = {
+    title: "Home",
+    ...navStyles
+  };
+
+  gotToPostPage = () => {
+    this.props.navigation.navigate('Post');
+  };
+
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <View style={styles.container}>
+          <Posts />
+          <Button onPress={this.gotToPostPage} title="Go to the Post Page" />
+        </View>
+      </ApolloProvider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default createStackNavigator({
+  Home: {
+    screen: App
+  },
+  Post : {
+    screen: Post
+  }
+});
---
+++ Post.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import navStyles from './styles/navStyles';
+
+export default class Post extends Component {
+  static navigationOptions = {
+    title: "Post",
+    ...navStyles
+  };
+
+  render() {
+    return (
+      <View>
+        <Text>New Page</Text>
+      </View>
+    );
+  }
+}
